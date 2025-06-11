47004036999aeac5d2af3174f525cb31cadc3a6c
Eduardoooxd <ecouto93@gmail.com>
2025-06-11 23:08:05 +0100

    chore: added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main.ts', {
+  timestamp: true,
+});
+
 async function bootstrap() {
+  logger.log(
+    `operation='bootstrap', message='Started bootstrap of application', environment='${process.env.NODE_ENV}'`,
+  );
+
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger({
       json: process.env.NODE_ENV === 'production',
@@ -23,6 +32,21 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
     credentials: true,
   });
+
+  // Enable swaggerUI for local and dev environments
+  // Access at localhost:3000/swaggerui
+  if (['local', 'development'].includes(process.env.NODE_ENV!)) {
+    logger.log(`operation='bootstrap', message='Enabled swagger ui'`);
+
+    const config = new DocumentBuilder()
+      .setTitle('Zap Table BE API')
+      .setDescription('Zap Table Management API')
+      .setVersion('1.0')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swaggerui', app, documentFactory);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
